49402a329de61eb462087edadc02363570c4987d
Zekai Tombul <zekaitombul@gmail.com>
2025-04-09 01:15:49 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // dış IP'lerden erişim için
+    port: 3000       // Render veya başka platformların yaygın portu
+  },
   test: {
     globals: true,
     environment: 'jsdom',
